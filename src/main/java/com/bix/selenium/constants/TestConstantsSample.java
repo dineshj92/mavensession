04e467ea56efea4e33a0fb5b3952c67344ba3280
djayakum <dineshj92@yahoo.com>
2019-01-26 16:48:54 +0530

    seventhcommit

--- src/main/java/com/bix/selenium/constants/TestConstantsSample.java
+++ src/main/java/com/bix/selenium/constants/TestConstantsSample.java
@@ -1,9 +1,9 @@
 package com.bix.selenium.constants;
 
-public class TestConstants {
+public class TestConstantsSample {
 	
 	public static String URL = "https://www.facebook.com/";
-	public static String keywordFile = "./resources/keyword/keywords_keywords.xls";
+	public static String keywordFile = "./resources/keyword/keywords_conflict.xls";
 	public static String keywordFile_reflection = "./resources/keyword/keywords_reflection.xls";
 	public static String[] scenarios = {"Fillsignupdetails", "Fillsignupdetails"};
 
